2b21e20e3208ecdf60e7c9f5176fae92ba3becae
jerremeirago <jrago@melistechnology.com>
2018-01-19 11:55:42 +0800

    added tool tip translations

--- language/fr_FR.install.php
+++ language/fr_FR.install.php
@@ -4,10 +4,16 @@ return array(
     //Platform Id
     'tr_melis_installer_platform_id' => 'Platform id',
     'tr_melis_installer_pids_page_id_start' => 'Page id start',
+    'tr_melis_installer_pids_page_id_start_info' => 'Page id start info',
     'tr_melis_installer_pids_page_id_current' => 'Page id current',
-    'tr_melis_installer_pids_tpl_id_start' => 'Tpl id start',
+    'tr_melis_installer_pids_page_id_current_info' => 'Page id current info',
     'tr_melis_installer_pids_page_id_end' => 'Tpl id end',
+    'tr_melis_installer_pids_page_id_end_info' => 'Tpl id end info',
+    'tr_melis_installer_pids_tpl_id_start' => 'Tpl id start',
+    'tr_melis_installer_pids_tpl_id_start_info' => 'Tpl id start info',
     'tr_melis_installer_pids_tpl_id_current' => 'Tpl id current',
+    'tr_melis_installer_pids_tpl_id_current_info' => 'Tpl id current info',
     'tr_melis_installer_pids_tpl_id_end' => 'Tpl id end',
+    'tr_melis_installer_pids_tpl_id_end_info' => 'Tpl id end info',
 
 );
