b534264fb2e50916b9f6242c2fe75825f27eab5e
DESKTOP-E78O66O\cmaka <crismakabenta@gmail.com>
2018-01-29 18:25:48 +0800

    create new get data function

--- src/Model/Tables/MelisSite301Table.php
+++ src/Model/Tables/MelisSite301Table.php
@@ -18,4 +18,41 @@ class MelisSite301Table extends MelisGenericTable
 		parent::__construct($tableGateway);
 		$this->idField = 's301_id';
 	}
+
+    public function getData($search = '', $siteId = null,  $searchableColumns = [], $orderBy = '', $orderDirection = 'ASC', $start = 0, $limit = null)
+    {
+        $select = $this->tableGateway->getSql()->select();
+        $select->columns(array('*'));
+        $select->join('melis_cms_site', 'melis_cms_site.site_id = melis_cms_site_301.s301_site_id',
+            array('site_name'), $select::JOIN_LEFT);
+
+        if(!empty($searchableColumns) && !empty($search)) {
+            foreach($searchableColumns as $column) {
+                $select->where->or->like($column, '%'.$search.'%');
+            }
+        }
+
+        if(!empty($siteId) && !is_null($siteId)){
+            $select->where->equalTo("s301_site_id", $siteId);
+        }
+
+        if(!empty($orderBy)) {
+            $select->order($orderBy . ' ' . $orderDirection);
+        }
+
+        $getCount = $this->tableGateway->selectWith($select);
+        // set current data count for pagination
+        $this->setCurrentDataCount((int) $getCount->count());
+
+        if(!empty($limit)) {
+            $select->limit( (int) $limit);
+        }
+
+        if(!empty($start)) {
+            $select->offset( (int) $start);
+        }
+
+        $resultSet = $this->tableGateway->selectWith($select);
+        return $resultSet;
+    }
 }
