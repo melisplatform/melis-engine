663e61c2b31c5e8a2efbcad6cd288d5ad2019656
jerremeirago <jrago@melistechnology.com>
2018-02-12 17:01:18 +0800

    removed site saving code

--- src/Controller/MelisSetupController.php
+++ src/Controller/MelisSetupController.php
@@ -126,43 +126,6 @@ class MelisSetupController extends AbstractActionController
                     $tableSiteDomain = $this->getServiceLocator()->get('MelisEngineTableSiteDomain');
                     $tableSite = $this->getServiceLocator()->get('MelisEngineTableSite');
 
-                  //  $cmsSiteSrv = $this->getServiceLocator()->get('MelisCmsSiteService');
-//
-//                    $container = new \Zend\Session\Container('melisinstaller');
-//                    $selectedSite = isset($container['site_module']['site']) ? $container['site_module']['site'] : null;
-//
-//                    $environments = isset($container['environments']['new']) ? $container['environments']['new'] : null;
-//                    $siteId = 1;
-//
-//                    if ($selectedSite) {
-//                        if ($selectedSite == 'NewSite') {
-//
-//                            $dataSite = array(
-//                                'site_name' => $selectedSite['cms_data']['web_form']['website_name']
-//                            );
-//
-//                            $dataDomain = array(
-//                                'sdom_env' => $environmentName,
-//                                'sdom_scheme' => $scheme,
-//                                'sdom_domain' => $siteDomain
-//                            );
-//
-//                            $dataSiteLang = $selectedSite['cms_data']['web_lang'];
-//
-//                            $genSiteModule = true;
-//
-//                            $siteModule = getenv('MELIS_MODULE');
-//
-//                           // $saveSiteResult = $cmsSiteSrv->saveSite($dataSite, $dataDomain, array(), $dataSiteLang, null, $genSiteModule, $siteModule);
-//
-//                            if ($saveSiteResult['success']) {
-//                                $siteId = $saveSiteResult['site_id'];
-//                            }
-//                        }
-//                    }
-
-                  //  $this->saveCmsSiteDomain($scheme, $siteDomain);
-
 
                     $success = 1;
                     $message = 'tr_install_setup_message_ok';
