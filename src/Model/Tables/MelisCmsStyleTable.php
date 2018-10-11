6bb9fad1bc73660c8eb5ec0223b7191ce649a39d
bryan <rbbrioso28@gmail.com>
2018-10-11 15:50:00 +0800

    Page style tool table list query

--- src/Model/Tables/MelisCmsStyleTable.php
+++ src/Model/Tables/MelisCmsStyleTable.php
@@ -37,4 +37,46 @@ class MelisCmsStyleTable extends MelisGenericTable
 	    
 	    return $resultSet;
 	}
+
+    /**
+     * @param string $search
+     * @param array $searchableColumns
+     * @param string $orderBy
+     * @param string $orderDirection
+     * @param int $start
+     * @param null $limit
+     * @return mixed
+     */
+    public function getStyleList($search = '', $searchableColumns = [], $orderBy = '', $orderDirection = 'ASC', $start = 0, $limit = null)
+    {
+        $select = $this->tableGateway->getSql()->select();
+        $select->columns(array('*'));
+
+        $select->join('melis_cms_site', 'site_id = style_site_id', array(), $select::JOIN_LEFT);
+
+        if(!empty($searchableColumns) && !empty($search)) {
+            foreach($searchableColumns as $column) {
+                $select->where->or->like($column, '%'.$search.'%');
+            }
+        }
+
+        if(!empty($orderBy)) {
+            $select->order($orderBy . ' ' . $orderDirection);
+        }
+
+        $getCount = $this->tableGateway->selectWith($select);
+        // set current data count for pagination
+        $this->setCurrentDataCount((int) $getCount->count());
+
+        if(!empty($limit)) {
+            $select->limit($limit);
+        }
+
+        if(!empty($start)) {
+            $select->offset($start);
+        }
+
+        $resultSet = $this->tableGateway->selectWith($select);
+        return $resultSet;
+    }
 }
