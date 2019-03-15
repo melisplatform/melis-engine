293a4ff575fa6842c672688cee437995675069c7
Cris <crismakabenta@gmail.com>
2019-03-15 15:53:12 +0800

    Change field name

--- install/dbdeploy/1031519_engine_update.sql
+++ install/dbdeploy/1031519_engine_update.sql
@@ -4,7 +4,7 @@
 
 CREATE TABLE IF NOT EXISTS `melis_cms_site_langs​` (
   `slang_id` int(11) NOT NULL AUTO_INCREMENT,
-  `slang_side_id` int(11) NOT NULL,
+  `slang_site_id` int(11) NOT NULL,
   `slang_lang_id` int(11) NOT NULL,
   PRIMARY KEY (`slang_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
