0033fe21ecbfffc761ec1df625afb94c6d8c1e59
Cris <crismakabenta@gmail.com>
2019-04-03 12:26:40 +0800

    Add site config table and rename sql file name

--- install/dbdeploy/1031519_cms_site_langs_install.sql
+++ install/dbdeploy/1031519_cms_site_langs_install.sql
@@ -1,12 +1,11 @@
 --
--- Table structure for table `melis_cms_site_lang`
+-- Table structure for table `melis_cms_site_langs`
 --
 
 CREATE TABLE IF NOT EXISTS `melis_cms_site_langs` (
   `slang_id` int(11) NOT NULL AUTO_INCREMENT,
   `slang_site_id` int(11) NOT NULL,
   `slang_lang_id` int(11) NOT NULL,
+  `slang_status` tinyint(4) NOT NULL DEFAULT '1',
   PRIMARY KEY (`slang_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-ALTER TABLE `melis_cms_site_langs` ADD `slang_status` TINYINT NOT NULL DEFAULT '1' AFTER `slang_lang_id`;
