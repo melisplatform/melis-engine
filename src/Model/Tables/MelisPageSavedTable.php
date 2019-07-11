aac68eef7583a2fb77e438b0fb4d572c2386782b
Kevin Suson <ksuson@melistechnology.com>
2019-07-11 17:54:11 +0800

    Saved & Published pages under Preview selector

--- src/Model/Tables/MelisPageSavedTable.php
+++ src/Model/Tables/MelisPageSavedTable.php
@@ -29,4 +29,36 @@ class MelisPageSavedTable extends MelisGenericTable
 	    
 	    return $resultSet;
 	}
+
+    /**
+     * Retrieve pages by type & per site
+     *
+     * @param string|null $pageType
+     * @param int|null $siteId
+     * @return \Zend\Db\ResultSet\ResultSetInterface
+     */
+    public function getPagesByType(string $pageType = null, int $siteId = null)
+    {
+        $select = $this->tableGateway->getSql()->select();
+
+        if (!empty($pageType)) {
+            $select->where->equalTo('page_type', $pageType);
+        }
+
+        if (!empty($siteId)) {
+            $select->join(
+                'melis_cms_template',
+                'melis_cms_template.tpl_id = melis_cms_page_saved.page_tpl_id',
+                [
+                    'tpl_id',
+                    'tpl_site_id',
+                    'tpl_zf2_website_folder',
+                ],
+                $select::JOIN_LEFT
+            );
+            $select->where->equalTo('tpl_site_id', $siteId);
+        }
+
+        return $this->tableGateway->selectWith($select);
+    }
 }
