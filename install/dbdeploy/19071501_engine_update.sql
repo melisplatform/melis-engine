834434f8a5696908f0ec6310a00391c8bf1eda2b
Kevin Suson <ksuson@melistechnology.com>
2019-07-15 19:19:11 +0800

    Additional Page type: Blog details

---
+++ install/dbdeploy/19071501_engine_update.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `melis_cms_page_published` CHANGE `page_type` `page_type` ENUM('SITE', 'FOLDER', 'PAGE', 'NEWSLETTER', 'NEWS_DETAIL', 'BLOG_DETAIL') NOT NULL DEFAULT 'PAGE';
+ALTER TABLE `melis_cms_page_saved` CHANGE `page_type` `page_type` ENUM('SITE', 'FOLDER', 'PAGE', 'NEWSLETTER', 'NEWS_DETAIL', 'BLOG_DETAIL') NOT NULL DEFAULT 'PAGE';
