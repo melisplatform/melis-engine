64b65490e469cfe448ace78cf0e73304278e3d13
unknown <crismakabenta@gmail.com>
2020-05-20 18:12:59 +0800

    Add demo cms twig on creation of domain

--- src/Controller/MelisSetupPostDownloadController.php
+++ src/Controller/MelisSetupPostDownloadController.php
@@ -180,7 +180,8 @@ class MelisSetupPostDownloadController extends AbstractActionController implemen
                         }
                     }else{
                         //create domain only if the user install MelisDemoCms or MelisDemoCmsTwig or other sites
-                        if($selectedSite == 'MelisDemoCms')
+                        $sitesList = ['MelisDemoCms', 'MelisDemoCmsTwig'];
+                        if(in_array($selectedSite, $sitesList))
                             $this->saveCmsSiteDomain($scheme, $siteDomain);
                     }
                 }
